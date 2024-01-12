87f232ec1dbd21c3d0297c06d0072be5ac053eb7
Nicolas Esquilache <nicolas.esquilache@hotmail.com>
2024-01-12 16:34:34 -0300

    Proyecto estable

---
+++ src/app.js
@@ -0,0 +1,48 @@
+import express, { json } from 'express';
+import ProductManager from './ProductManager.js';
+
+const app = express();
+const port = 8080;
+
+app.use(json());
+
+const productManager = new ProductManager();
+
+app.get("/products", async (req, res) => {
+    try {
+        const productos = await productManager.getProducts();
+        let limit = parseInt(req.query.limit);
+        let productosLimitados = [...productos];
+
+
+        if (!isNaN(limit) && limit > 0) {
+            productosLimitados = productosLimitados.slice(0, limit);
+        }
+
+        res.send(productosLimitados);
+    } catch (error) {
+        console.error("Error al obtener los productos:", error.message);
+    }
+});
+
+app.get("/products/:idProduct", async (req, res) => {
+
+    try {
+        const productos = await productManager.getProducts();
+        let idProduct = parseInt(req.params.idProduct);
+        console.log(idProduct);
+        let producto = productos.find(u => u.id === idProduct);
+
+        if (!producto) return res.send({ error: "producto no encontrado" });
+
+        res.send(producto);
+    } catch (error) {
+        console.error("Error al obtener el producto:", error.message);
+    }
+
+
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`);
+});
