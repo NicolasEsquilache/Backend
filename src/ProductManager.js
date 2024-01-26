5ca730c2aa079f784a4ab3bfdf5fc778f4b9bd88
Nicolas Esquilache <nicolas.esquilache@hotmail.com>
2024-01-26 18:32:13 -0300

    Primera Pre-entrega

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,5 +1,4 @@
 import { promises as fs } from 'fs';
-import path from 'path';
 
 export default class ProductManager {
     constructor(path) {
@@ -8,81 +7,38 @@ export default class ProductManager {
         this.path = path;
     }
 
-    async addProduct(producto) {
-
-        if (!producto.title || !producto.description || !producto.price || !producto.thumbnail || !producto.code || !producto.stock) {
-            console.error("Todos los campos son obligatorios.");
-            return;
-        }
-
-        if (this.productos.some((p) => p.code === producto.code)) {
-            console.error("El código ya existe.");
-            return;
-        }
-
-        producto.id = this.nextId++;
-        const { title, description, price, thumbnail, code, stock } = producto;
-
-        const nuevoProducto = {
-            id: producto.id,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock
-        }
-        this.productos.push(nuevoProducto);
-
-        try {
-            await fs.writeFile(this.path, JSON.stringify(this.productos), "utf-8");
-            return nuevoProducto;
-        } catch (error) {
-            console.error("Error al escribir en el archivo:", error);
-        }
-    }
-
     async getProducts() {
         try {
-            const data = await fs.readFile(this.path, "utf-8");
-            if (!data) {
-                return [];
-            } else {
-                return JSON.parse(data);
-            }
-
+            const data = await fs.readFile(this.path, 'utf-8');
+            return data ? JSON.parse(data) : [];
         } catch (error) {
-            console.error("Error al leer el archivo:", error);
+            console.error('Error al leer el archivo:', error);
             return [];
         }
     }
 
     async getProductById(id) {
         try {
-            const data = await fs.readFile(this.path);
+            const data = await fs.readFile(this.path, 'utf-8');
             const productos = JSON.parse(data);
             const producto = productos.find((p) => p.id === id);
-            if (producto) {
-                return producto;
-            } else {
-                throw new Error("Producto no encontrado");
-            }
+            if (!producto) throw new Error('Producto no encontrado');
+            return producto;
         } catch (error) {
             console.error(error.message);
+            throw error;
         }
     }
 
     async updateProduct(id, updatedProduct) {
         try {
             const data = await fs.readFile(this.path, 'utf-8');
-
             let productos = [];
+
             if (data) {
                 try {
                     productos = JSON.parse(data);
-                    if (!Array.isArray(productos)) {
-                        throw new Error('El archivo no contiene un arreglo JSON válido.');
-                    }
+                    if (!Array.isArray(productos)) throw new Error('El archivo no contiene un arreglo JSON válido.');
                 } catch (parseError) {
                     throw new Error('El archivo no contiene un formato JSON válido.');
                 }
@@ -90,13 +46,11 @@ export default class ProductManager {
 
             const productoIndex = productos.findIndex((p) => p.id === id);
 
-            if (productoIndex === -1) {
-                throw new Error('Producto no encontrado');
-            }
+            if (productoIndex === -1) throw new Error('Producto no encontrado');
 
             productos[productoIndex] = { ...productos[productoIndex], ...updatedProduct };
 
-            await fs.writeFile(this.path, JSON.stringify(productos), "utf-8");
+            await fs.writeFile(this.path, JSON.stringify(productos), 'utf-8');
 
             return productos[productoIndex];
         } catch (error) {
@@ -105,17 +59,57 @@ export default class ProductManager {
         }
     }
 
+    async addProduct(producto) {
+        try {
+            if (!producto.title || !producto.description || !producto.price || !producto.category || !producto.code || !producto.stock) {
+                console.error('Todos los campos son obligatorios.');
+                return;
+            }
+
+            let productos = JSON.parse(await fs.readFile(this.path, 'utf-8'));
+
+            if (productos.some((p) => p.code === producto.code)) {
+                console.error('El código ya existe.');
+                return;
+            }
+
+            const maxId = Math.max(...productos.map(p => p.id));
+
+            producto.id = maxId + 1;
+            const { title, description, code, price, stock, category, thumbnails } = producto;
+
+            const nuevoProducto = {
+                id: producto.id,
+                title,
+                description,
+                code,
+                price,
+                status: true,
+                stock,
+                category,
+                thumbnails: [thumbnails]
+            };
+
+            productos.push(nuevoProducto);
+
+            await fs.writeFile(this.path, JSON.stringify(productos), 'utf-8');
+
+            return nuevoProducto;
+        } catch (error) {
+            console.error('Error al agregar el producto:', error);
+            throw error;
+        }
+    }
+
     async deleteProduct(id) {
         try {
             const data = await fs.readFile(this.path, 'utf-8');
-
             let productos = [];
+
             if (data) {
                 try {
                     productos = JSON.parse(data);
-                    if (!Array.isArray(productos)) {
-                        throw new Error('El archivo no contiene un arreglo JSON válido.');
-                    }
+                    if (!Array.isArray(productos)) throw new Error('El archivo no contiene un arreglo JSON válido.');
                 } catch (parseError) {
                     throw new Error('El archivo no contiene un formato JSON válido.');
                 }
@@ -123,13 +117,11 @@ export default class ProductManager {
 
             const productoIndex = productos.findIndex((p) => p.id === id);
 
-            if (productoIndex === -1) {
-                throw new Error('Producto no encontrado');
-            }
+            if (productoIndex === -1) throw new Error('Producto no encontrado');
 
             const productoEliminado = productos.splice(productoIndex, 1)[0];
 
-            await fs.writeFile(this.path, JSON.stringify(productos));
+            await fs.writeFile(this.path, JSON.stringify(productos), 'utf-8');
 
             return productoEliminado;
         } catch (error) {
--- src/app.js
+++ src/app.js
@@ -1,47 +1,14 @@
-import express, { json } from 'express';
-import ProductManager from './ProductManager.js';
-
+import express from 'express';
+import productRouter from './routes/product.router.js'
+import cartRouter from './routes/cart.router.js'
 const app = express();
 const port = 8080;
 
-app.use(json());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-const productManager = new ProductManager("src/products.json");
-
-app.get("/products", async (req, res) => {
-    try {
-        const productos = await productManager.getProducts();
-        let limit = parseInt(req.query.limit);
-        let productosLimitados = [...productos];
-
-
-        if (!isNaN(limit) && limit > 0) {
-            productosLimitados = productosLimitados.slice(0, limit);
-        }
-
-        res.send(productosLimitados);
-    } catch (error) {
-        console.error("Error al obtener los productos:", error.message);
-    }
-});
-
-app.get("/products/:idProduct", async (req, res) => {
-
-    try {
-        const productos = await productManager.getProducts();
-        let idProduct = parseInt(req.params.idProduct);
-        console.log(idProduct);
-        let producto = productos.find(u => u.id === idProduct);
-
-        if (!producto) return res.send({ error: "producto no encontrado" });
-
-        res.send(producto);
-    } catch (error) {
-        console.error("Error al obtener el producto:", error.message);
-    }
-
-
-});
+app.use("/api/products",productRouter);
+app.use("/api/carts",cartRouter);
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en el puerto ${port}`);
